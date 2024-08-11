9d01f6d549500ac95fc32b4c28429bbf2e02ea02
Anish09122 <anishppthakur30@gmail.com>
2024-08-11 13:14:03 +0530

    first commit

---
+++ src/helpers/Questions.js
@@ -0,0 +1,26 @@
+export const Questions = [
+    {
+      prompt: "First Page of website is termed as?",
+      optionA: "index page",
+      optionB: "HomePage",
+      optionC: "Sitemap",
+      optionD: "Pen Drive",
+      asnwer: "optionB",
+    },
+    {
+      prompt: "Which of this is not a programming language?",
+      optionA: "Python",
+      optionB: "JavaScript",
+      optionC: "MC-03",
+      optionD: "Java",
+      asnwer: "optionC",
+    },
+    {
+      prompt: "Which of this is not a javascript framework?",
+      optionA: "React",
+      optionB: "Angular",
+      optionC: "Vue",
+      optionD: "Java",
+      asnwer: "optionD",
+    },
+  ];
